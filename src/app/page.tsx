0c9f3016a80c11604115dc3f576fce41f5df0981
Rajan Patel <88902043+rajanpatel126@users.noreply.github.com>
2024-02-22 17:22:21 +0530

    Hero section completed with 2 Aceternity UI components included.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
    return (
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.20]">
-         <h1 className="text-2xl text-center border-2 border-white">
-            MusixTech Launch
-         </h1>
+         <HeroSection />
       </main>
    );
 }
